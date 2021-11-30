a4c10a5adbd7da13be0f8c5aa28ea6b574a2fb2b
5uh4 <a_suhail.matrouch.mohamed@iespablopicasso.es>
2021-11-30 12:40:13 +0100

    Archivos con array sin impresion

---
+++ src/act3GIT/Act3GIT_Suhail.java
@@ -0,0 +1,16 @@
+package act3GIT;
+
+public class Act3GIT_Suhail {
+
+	public static void main(String[] args) {
+		System.out.println("20 números aleatorios entre 1 y 10.");
+		int[] a;
+		a = new int[20];
+		int i;
+		
+		for (i = 0; i < a.length - 1; i++) {
+			a[i] = ((int)Math.random() * 10);
+		}
+	}
+
+}
